fc2edfc1c9ebbac0240968fd600cc27d3fd3f124
Luis-Enrique-NR <luis.enrique.nunez.ramos@gmail.com>
2025-08-12 23:13:52 -0500

    Se agregó el proyecto SpringBoot

--- web-app/backend/foodfactory/src/main/java/pe/edu/uni/foodfactory/presentation/dto/RegistrarPersonaRequestDTO.java
+++ web-app/backend/foodfactory/src/main/java/pe/edu/uni/foodfactory/presentation/dto/RegistrarPersonaRequestDTO.java
@@ -25,17 +25,4 @@ public class RegistrarPersonaRequestDTO {
     private String direccion;
     private LocalDate fechaNacimiento;
     private String codigoUbigeo;
-
-
-    /*
-    id_persona SERIAL PRIMARY KEY,
-    dni CHAR(8) NOT NULL UNIQUE,
-    nombre VARCHAR(50) NOT NULL,
-    ap_paterno VARCHAR(50) NOT NULL,
-    ap_materno VARCHAR(50) NOT NULL,
-    direccion VARCHAR(100),
-    ubigeo_direccion CHAR(6),
-    fecha_nac DATE,
-    FOREIGN KEY (ubigeo_direccion) REFERENCES Ubigeos(id_ubigeo)
-     */
 }
